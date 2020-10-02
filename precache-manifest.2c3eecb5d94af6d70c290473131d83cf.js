d4dbddedf0b24a7fc1f1fa062e896c23ce46f68d
Frank <fgunsch.webdev@gmail.com>
2020-10-02 19:44:32 -0400

    Updates

---
+++ precache-manifest.2c3eecb5d94af6d70c290473131d83cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e95d4fb2c4244b1ff05d6820d5337bcf",
+    "url": "/pingpongapp/index.html"
+  },
+  {
+    "revision": "e666012d55abf197c69b",
+    "url": "/pingpongapp/static/css/main.c4969732.chunk.css"
+  },
+  {
+    "revision": "ee16d47897042fd2a07d",
+    "url": "/pingpongapp/static/js/2.e5656570.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pingpongapp/static/js/2.e5656570.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e666012d55abf197c69b",
+    "url": "/pingpongapp/static/js/main.316b61c6.chunk.js"
+  },
+  {
+    "revision": "82d715d0dd317678816c",
+    "url": "/pingpongapp/static/js/runtime-main.1f5af3db.js"
+  }
+]);
